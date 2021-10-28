9e1fde25f05fb7336bdc4f4d75ccfb70ef23d7d7
Maratatasan <marats.stelihs@gmail.com>
2021-10-28 14:44:02 +0100

    added cors

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,9 @@
 // import { Request, Response } from "express"
 const express = require("express");
+// const cors = require('cors');
 const app = express();
 const port = 8000;
+// app.use(cors({}))
 app.get("/", (request, response) => {
     response.send("hello world");
 });
--- server.ts
+++ server.ts
@@ -1,9 +1,10 @@
 // import { Request, Response } from "express"
 const express = require("express")
+const cors = require('cors');
 
 const app = express()
 const port = 8000
-
+app.use(cors({}))
 app.get("/",(request: any, response: any)=>{
     response.send("hello world")
 })
