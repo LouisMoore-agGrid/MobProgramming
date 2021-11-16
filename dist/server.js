5655a16ceddb8acc934d2df36351cb8d8ad8e676
bamfard@gmail.com <^,+jTk'(wiq"ybh&Ga5[y>
2021-11-16 13:56:48 +0000

    created endpoints for adding and getting games

--- dist/server.js
+++ dist/server.js
@@ -1,11 +1,22 @@
+// import { request } from "chai";
 // import { Request, Response } from "express"
 const express = require("express");
-// const cors = require('cors');
+const cors = require('cors');
 const app = express();
 const port = 8000;
-// app.use(cors({}))
-app.get("/", (request, response) => {
-    response.send("hello world");
+app.use(cors({}));
+app.use(express.json());
+const games = [
+    'Call of Shuheb!'
+];
+app.get("/games", (request, response) => {
+    response.send(games);
+});
+app.post("/addGame", (request, response) => {
+    // const newText = request.body;
+    let newGame = request.body.newGame;
+    games.push(newGame);
+    response.send(games);
 });
 app.listen(port, () => {
     console.log('server is now listening on port:', port);
--- server.ts
+++ server.ts
@@ -1,3 +1,5 @@
+// import { request } from "chai";
+
 // import { Request, Response } from "express"
 const express = require("express")
 const cors = require('cors');
@@ -5,9 +7,27 @@ const cors = require('cors');
 const app = express()
 const port = 8000
 app.use(cors({}))
-app.get("/",(request: any, response: any)=>{
-    response.send("hello world")
+app.use(express.json()) 
+
+
+const games = [
+    'Call of Shuheb!'
+];
+
+app.get("/games",(request: any, response: any)=>{
+    response.send(games)
 })
+
+app.post("/addGame", (request: any, response: any)=> {
+    // const newText = request.body;
+    let newGame = request.body.newGame;
+    games.push(newGame);
+    response.send(games);
+    
+
+    
+})
+
 app.listen(port,()=>{
     console.log('server is now listening on port:', port)
 })
